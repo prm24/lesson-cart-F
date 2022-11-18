de5a5b4b5803b62f538663241dc38d40b1e9697e
prym001 <pp843@live.mdx.ac.uk>
2022-11-18 11:43:31 +0000

    sorting functionality

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,78 @@
   <div id="app">
     <div id="app" class="my-5 flex-container">
       <div class="sidenav">
-        Sort By
-        <ul>
-          <li>
-            
-          </li>
-          </ul>
+        <div>
+          Sort By
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="sortBy" v-model="selectedText" checked id="subject"
+              :value="'name'">
+            <label class="form-check-label" for="subject">
+              Subject
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="sortBy" v-model="selectedText" id="location"
+              :value="'location'">
+            <label class="form-check-label" for="location">
+              Location
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="sortBy" v-model="selectedText" id="price"
+              :value="'price'">
+            <label class="form-check-label" for="price">
+              Price
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="sortBy" v-model="selectedText" id="availability"
+              :value="'spaces'">
+            <label class="form-check-label" for="availability">
+              Availability
+            </label>
+          </div>
+        </div>
+        <div class="mt-3">
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="ascDec" v-model="selectedText" id="ascending" :value=1>
+            <label class="form-check-label" for="ascending">
+              Ascending
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="ascDec" v-model="selectedText" id="descending" :value=2>
+            <label class="form-check-label" for="descending">
+              Descending
+            </label>
+          </div>
+        </div>
       </div>
       <div class="content">
         <h1>My online store</h1>
         <div class="container mx-auto mt-4">
 
-      
-        <div class="d-flex flex-wrap w-100 justify-content-between">
-          <div class=" me-1 card" v-for="(product, index) in listOfProduct">
-            <div class="card-body card-h">
-              <!-- <h5 class="card-title">{{ product.name }}</h5> -->
-              <img src="./assets/Your-Life-in-Pictures-Cover.jpg" class="card-img-top" alt="..." />
-              <div class="mt-2 card-body-content d-flex flex-column justify-content-between">
-                <div class="mt-2 ">
-                  <h6 class="card-subtitle mb-2 text-muted">Name: {{ product.name }}</h6>
-                  <h6 class="card-subtitle mb-2 text-muted">Location: {{ product.location }}</h6>
-                  <h6 class="card-subtitle mb-2 text-muted">Price: {{ product.price }} </h6>
-                  <h6 class="card-subtitle mb-2 text-muted">Spaces : {{ product.spaces }}</h6>
-                </div>
-                <div>
-                  <button class="btn btn-primary" :disabled=isBtn @click="addToCart(product, index)">Add to cart
-                  </button>
+
+          <div class="d-flex flex-wrap w-100 justify-content-between">
+            <div class=" me-1 card" v-for="(product, index) in listOfProduct">
+              <div class="card-body card-h">
+                <!-- <h5 class="card-title">{{ product.name }}</h5> -->
+                <img src="./assets/Your-Life-in-Pictures-Cover.jpg" class="card-img-top" alt="..." />
+                <div class="mt-2 card-body-content d-flex flex-column justify-content-between">
+                  <div class="mt-2 ">
+                    <h6 class="card-subtitle mb-2 text-muted">Name: {{ product.name }}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Location: {{ product.location }}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Price: {{ product.price }} </h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Spaces : {{ product.spaces }}</h6>
+                  </div>
+                  <div>
+                    <button class="btn btn-primary" :disabled=isBtn @click="addToCart(product, index)">Add to cart
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
         <h3 class="mt-3">Shopping Cart</h3>
         <div class="d-flex flex-wrap w-100 justify-content-between">
           <div class=" me-1 card" v-for="(product, index) in selectedProduct">
@@ -79,6 +118,7 @@ export default {
       listOfProduct: [],
       selectedProduct: [],
       toShowSelectedProduct: [],
+      sortSelected: 1,
       form: {
         name: "",
         number: ""
@@ -90,6 +130,36 @@ export default {
     //   name: { required },
     //   number: { required }
     // }
+  },
+  computed: {
+    selectedText: {
+      get() {
+        return this.sortSelected;
+      },
+      set(newValue) {
+        if (typeof newValue === 'string') {
+          this.listOfProduct.sort((a, b) => {
+
+            let fa = typeof a == 'string' ? a[newValue].toLowerCase() : a[newValue];
+            let fb = typeof a == 'string' ? b[newValue].toLowerCase() : b[newValue];
+            if (fa < fb) {
+              return -1
+            }
+            if (fa > fb) {
+              return 1
+            }
+            return 0
+          });
+        }else{
+          debugger
+          if(newValue==1){
+            this.listOfProduct=this.listOfProduct.sort()
+          }else{
+            this.listOfProduct=this.listOfProduct.reverse()
+          }
+        }
+      }
+    }
   },
   created() {
 
@@ -156,10 +226,11 @@ export default {
 </script>
 
 <style>
-html, body{
-    margin: 0 !important;
-    padding: 0 !important;
-    width: 100%;
+html,
+body {
+  margin: 0 !important;
+  padding: 0 !important;
+  width: 100%;
 }
 
 #app {
@@ -183,12 +254,19 @@ html, body{
   -webkit-flex: 1;
   flex: 1;
 }
+
 .content {
   background-color: rgb(248, 253, 255);
   padding: 10px;
   -webkit-flex: 5;
   flex: 5;
   height: 2000px;
+}
+
+.form-check {
+  justify-content: space-between;
+  display: flex;
+  padding-left: 30px;
 }
 
 
